0ad406af8fee396464b713661f1264fd385aa142
dzbanek7 <adamgarnu@gmail.com>
2018-09-07 14:19:37 +0200

    c++ i python

---
+++ Python/szablon.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  szablon.py
+
+
+def main(args):
+    #a = 0 # inicjacja zmiennnej
+    a = input('Podaj liczbę: ')
+    print(a)
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
---
+++ cpp/szablon.cpp
@@ -0,0 +1,19 @@
+/*
+ * szablon.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int a; //deklaracja zmiennej
+    a = 0; //inicjacja zmiennej
+    cout << "Podaj liczbę ";
+    cin >> a;
+    cout << a;
+	return 0;
+}
+
