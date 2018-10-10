62c04238c4207b84ec5cb9946dcbf3a78b791606
dzbanek7 <adamgarnu@gmail.com>
2018-10-10 17:07:58 +0200

    dodanie liczby_3

--- algorytmy/liczby23.py
+++ algorytmy/liczby23.py
@@ -20,9 +20,28 @@ def liczby2():
                 ile = ile + 1
     
     return ile
+    
+def liczby3():
+    """
+    Funkcja drukuje wszystkie liczby 3-cyfrowe, w których cyfry nie powtarzają się. Funkcja zwraca ich liczbę.
+    Wykluczone liczby: 111, 222, 333, 110, 211 itd.
+    """
+    
+    ile = 0
+    
+    for i in range(1, 10):
+        for j in range(0, 10):
+            for k in range(0, 10):
+                if i == j or i == k or k == j:
+                    print("", end = '')
+                else:
+                    print("{}{}{} ".format(i,j,k), end='')
+                    ile = ile + 1
+    
+    return ile
 
 def main(args):
-    print("Liczby 2-cyfrowych: ", liczby2())
+    print("Liczby 2-cyfrowych: ", liczby3())
     return 0
 
 if __name__ == '__main__':
