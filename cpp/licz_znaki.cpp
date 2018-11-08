9070cf889cfab8ca6b28008b3a9f62cf2f748480
dzbanek7 <adamgarnu@gmail.com>
2018-11-08 08:23:07 +0100

    dla mazura

--- cpp/licz_znaki.cpp
+++ cpp/licz_znaki.cpp
@@ -4,28 +4,40 @@
 
 
 #include <iostream>
+#include <string.h>
+
 using namespace std;
 
-void zamien_znaki(char tb[]. int roz) {
-        for(int i = 0; i < roz; i++) {
-            //zamień małe na duże;
-            kod_ascii 'a' = 97
-            kod ascii 'A' = 65
-            97 - 65 = 32
-            (char)(kod_ascii - 32)
-            todo: implementacja
-        }
+void zamiana_liter(char tab[])
+{   
+    int i = 0;
+    int kod = 0;
+    while(tab[i] != '\0') {
+        kod = (int)tab[i];
+        if (kod >= 65 && kod <= 90)
+            kod += 32;
+        else if (kod >= 97 && kod <= 122)
+            kod -= 32;
+        cout << (char)kod;  
+        i++;
+    }
 }
 
+
+
 void licz_znaki(char tb[], int roz) {
+    //~for(int i=0; i < roz; i++) {
+       //~cout << tb[i];
+    //~}
     int i = 0;
     int cyfry, literyD, literyM, reszta;
     cyfry = literyD = literyM = reszta = 0;
-    int znak_kod = 0; // kod ASCII badanego znaku
+    int znak_kod = 0; //kod ASCII badanego znaku
     
-    while (tb[i] != '\0') {
+    
+    while(tb[i] != '\0'){
         znak_kod = (int)tb[i];
-        if (znak_kod > 64 && znak_kod < 91)
+        if (znak_kod > 64 && znak_kod < 91) // to sprawia że sa duże litery
             literyD++;
         else if (znak_kod > 96 && znak_kod < 123)
             literyM++;
@@ -33,22 +45,40 @@ void licz_znaki(char tb[], int roz) {
             cyfry++;
         else 
             reszta++;
-
         
-        i++; //inkrementacja index-u
-    }
-    cout << "Duże: " << literyD << endl;
-    cout << "Małe: " << literyM << endl;
+    i++;  //inkrementcja indeksu
+   }
+    cout << "Duże litery: " << literyD << endl; 
+    cout << "Małe litery: " << literyM << endl; 
     cout << "Cyfry: " << cyfry << endl;
-    cout << "Reszta: " << reszta << endl;
+    cout << "Reszta: " << reszta << endl; 
 }
+
+int zlicz(char tb[]) {
+        int i = 0;
+        while(tb[i] != '\0') i++;
+        return i;
+}
+
+void ascii(char tb[], int roz) {
+    for(int i = 0; i < roz; i++) {
+            cout << (int)tb[i] << " ";
+    }
+}
+
 
 int main(int argc, char **argv)
-{   
+{
     const int rozmiar = 20;
-    char znaki[rozmiar];
+	char znaki[rozmiar];
     cin.getline(znaki, rozmiar);
-    licz_znaki(znaki, cin.gcount());
+    //licz_znaki(znaki, cin.gcount());
+    //zamiana_liter(znaki);
+    //~int ilosc = cin.gcount();
+    //~ilosc = zlicz(znaki);
+    int ilosc = 0;
+    ilosc = strlen(znaki);
+    ascii(znaki, ilosc);
+
     return 0;
 }
-
