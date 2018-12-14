62db2336585b6330ef7598133a596c250a6ebaa2
dzbanek7 <adamgarnu@gmail.com>
2018-12-14 13:29:39 +0100

    sortowwie

--- cpp/sortowanie.cpp
+++ cpp/sortowanie.cpp
@@ -15,11 +15,18 @@ void wypelnij(int tab[], int roz) {
 }
 
 void zamien1(int &a, int &b) {
-    cout << a << " " << b << endl;
+    //~cout << a << " " << b << endl;
     int tmp = a;
     a = b;
     b = tmp;
-    cout << a << " " << b << endl;
+    //~cout << a << " " << b << endl;
+}
+
+void zamien2(int tab[], int i) {
+    int tmp = tab[i];
+    tab[i] = tab[i + 1];
+    tab[i + 1] = tmp;
+    
 }
 
 void drukuj(int tab[], int roz) {
@@ -30,10 +37,11 @@ void drukuj(int tab[], int roz) {
 }
 
 void sort_bubble(int tab[], int n) {
-    for (;;) {
-        for (;;) {
-            //~if (tab[i] > tab[i+1])
-                //~zamien1(tab[i], tab[i+1]);
+    for (int j = n - 1; j > 0; j--) {
+        for (int i = 0; i < j; i++) {
+            if (tab[i] > tab[i+1])
+                //~zamien1(tab[i], tab[i + 1]);
+                zamien2(tab, i);
         }
     }
 }
@@ -44,10 +52,13 @@ int main(int argc, char **argv)
     int tab[roz];
     wypelnij(tab, roz);
     drukuj(tab, roz);
-    tab[0] = 7;
-    tab[1] = 5;
-    zamien1(tab[0], tab[1]);
-    cout << tab[0] << " " << tab[1];
-	return 0;
+    cout << endl;
+    //~tab[0] = 7;
+    //~tab[1] = 5;
+    //~zamien1(tab[0], tab[1]);
+    //~cout << tab[0] << " " << tab[1];
+	sort_bubble(tab, roz);
+    drukuj(tab, roz);
+    return 0;
 }
 
